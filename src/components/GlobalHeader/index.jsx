91e20e69e75a73dad3205b73b2ee42bc5d8cb6c3
ryantoddgarza <ryantoddgarza@gmail.com>
2022-11-09 10:59:33 -0600

    feat: basic build and style of global header & nav

--- src/components/GlobalHeader/index.jsx
+++ src/components/GlobalHeader/index.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import { Link } from 'gatsby';
+import { HOME_PATH } from '../../constants';
+import globalNav from '../../data/globalNav';
 
 const GlobalHeader = () => (
-  <header className="global-header section light"></header>
+  <header className="global-header section light">
+    <div className="item logo-container">
+      <div className="logo">
+        <Link to={HOME_PATH}></Link>
+      </div>
+      <div>
+        <Link to={HOME_PATH}>Textbooks</Link>
+      </div>
+    </div>
+    <nav className="item nav">
+      <ul className="list">
+        {globalNav.map(({ id, name, path }) => (
+          <li className="list-item" key={id}>
+            <Link className="link" to={path}>
+              {name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  </header>
 );
 
 export default GlobalHeader;
---
+++ src/data/globalNav.js
@@ -0,0 +1,21 @@
+import { STUDENTS_PATH, EDUCATORS_PATH, TEAM_PATH } from '../constants';
+
+const globalNav = [
+  {
+    id: 1,
+    name: 'Students',
+    path: STUDENTS_PATH,
+  },
+  {
+    id: 2,
+    name: 'Educators',
+    path: EDUCATORS_PATH,
+  },
+  {
+    id: 3,
+    name: 'Team',
+    path: TEAM_PATH,
+  },
+];
+
+export default globalNav;
