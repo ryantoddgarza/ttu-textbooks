7726ed4789afbfc9d2ec2e7348d629af2314ee53
ryantoddgarza <ryantoddgarza@gmail.com>
2022-11-09 09:36:23 -0600

    feat: add 404 page

---
+++ src/components/NotFound/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => (
+  <div className="main-content">
+    <section className="page-content text-align-center">
+      <h1>404</h1>
+      <p>Not found</p>
+    </section>
+  </div>
+);
+
+export default NotFound;
---
+++ src/pages/404.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '../components/Layout';
+import NotFound from '../components/NotFound';
+
+const NotFoundPage = () => (
+  <Layout>
+    <NotFound />
+  </Layout>
+);
+
+export default NotFoundPage;
