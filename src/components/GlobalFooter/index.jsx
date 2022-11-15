13f66bb951a1454d2f6b93eba85060e724133f66
ryantoddgarza <ryantoddgarza@gmail.com>
2022-11-15 10:57:57 -0600

    feat: add social list to footer

--- src/components/GlobalFooter/index.jsx
+++ src/components/GlobalFooter/index.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import { TEAM_PATH } from '../../constants';
+import { FaGithub } from 'react-icons/fa';
+import { TEAM_PATH, GITHUB_URL } from '../../constants';
 
 const GlobalFooter = () => (
   <footer className="global-footer section dark">
-    <div className="copyright">
+    <div className="item social">
+      <h2 className="heading">Follow the Project</h2>
+      <ul className="list">
+        <li>
+          <div className="icon">
+            <a
+              className="link light"
+              href={GITHUB_URL}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaGithub />
+            </a>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <div className="item copyright">
       <Link to={TEAM_PATH}>{`© ${new Date().getFullYear()} Project Team`}</Link>
     </div>
   </footer>
--- src/constants/index.js
+++ src/constants/index.js
@@ -6,3 +6,4 @@ exports.TEAM_PATH = '/team';
 
 // URLs
 exports.STUDENT_SURVEY_URL = 'https://forms.gle/qmQmnfZQYha1o6ys5';
+exports.GITHUB_URL = 'https://github.com/ryantoddgarza/ttu-textbooks.git';
