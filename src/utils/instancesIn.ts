835d9b89514d3e5521f1850efc488a2925f567ca
ryantoddgarza <ryantoddgarza@gmail.com>
2022-11-30 22:47:58 -0600

    feat: add option to create instances from string array

--- src/utils/instancesIn.ts
+++ src/utils/instancesIn.ts
@@ -1,8 +1,29 @@
 // Returns an object with instances of values given an array.
 
-const instancesIn = (arr: (string | number)[]): Record<string, never> => {
+const instancesIn = (
+  arr: (string | number)[],
+  check?: string[]
+): Record<string, never> => {
   return arr.reduce((all, curr) => {
-    const currCount = all[curr] ?? 0;
+    let currCount = all[curr] ?? 0;
+
+    // Instance(s) from string(s) in check array
+    if (check && typeof curr === 'string') {
+      const found = {};
+
+      for (const str of check) {
+        if (curr.includes(str)) {
+          currCount = all[str] ?? 0;
+          found[str] = currCount + 1;
+        }
+      }
+
+      return {
+        ...all,
+        ...found,
+      };
+    }
+
     return {
       ...all,
       [curr]: currCount + 1,
